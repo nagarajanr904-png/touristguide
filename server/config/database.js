eb151e79bc8d24cc5b151982fa08c3003648987d
Nagarajan.R <nagarajanr904@gmail.com>
2026-01-09 20:09:24 +0530

    Update DB config for cloud

--- server/config/database.js
+++ server/config/database.js
@@ -7,8 +7,15 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
-        logging: false
+        logging: false,
+        dialectOptions: process.env.DB_SSL === 'true' ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Common for self-signed certificates in dev/mvp
+            }
+        } : {}
     }
 );
 
